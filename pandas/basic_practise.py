c7d7bd32081d570f5b210df7658bd529908ed237
Sarbuland <meharsahab435@gmail.com>
2025-11-12 23:27:17 +0500

    practise

--- pandas/basic_practise.py
+++ pandas/basic_practise.py
@@ -1,28 +1,28 @@
 #  Read CSV file
 import pandas as pd
-# df=pd.read_csv("CSV_Files/pandas_practice.csv")
-# print(df)
-# print()
+df=pd.read_csv("CSV_Files/pandas_practice.csv")
+print(df)
+print()
 # # Read Excel file
-# df=pd.read_excel("Excel_file/pandas_practice.xlsx")
-# print(df)
-# print()
+df=pd.read_excel("Excel_file/pandas_practice.xlsx")
+print(df)
+print()
 # # Read Jason file
-# df=pd.read_json("jason file/pandas_practice.json")
-# print(df)
+df=pd.read_json("jason file/pandas_practice.json")
+print(df)
 
 # create file
-# data={
-#     "Name":["ali","ahmad","kamal"],
-#     "Age":[23,21,22],
-#     "City":["lahore","karachi","Talwandi"]
-# }
-# df=pd.DataFrame(data)
-# df.to_csv("output.csv",index=False)
-# print(df)
-# print()
-# df2=pd.read_csv("output.csv")
-# print(df2)
+data={
+    "Name":["ali","ahmad","kamal"],
+    "Age":[23,21,22],
+    "City":["lahore","karachi","Talwandi"]
+}
+df=pd.DataFrame(data)
+df.to_csv("output.csv",index=False)
+print(df)
+print()
+df2=pd.read_csv("output.csv")
+print(df2)
 # Asy hi ya excel may bi ho sakta ha or jason may bi
 
 # now head() and tail() concept
@@ -45,3 +45,28 @@ print(df.describe())
 print()
 print(df.shape)
 print(df.columns)
+data={
+    "Name":["ali","ahmad","kamal","talwar khan","azad khan"],
+    "Age":[23,21,22,21,22],
+    "City":["lahore","karachi","Talwandi","kasur","islamabad"],
+    "salary":[2000,23000,56000,45000,10000]
+}
+df=pd.DataFrame(data)
+print(df)
+print()
+# sigle column ko access karna ha ab
+print(df["Name"])
+# now multiple 
+print()
+col=df[["Name","Age"]]
+print(col)
+
+# now we filter the rows
+col=df[df["salary"]>5000]
+print(col)
+print()
+col=df[(df["salary"]>10000) | (df["Age"]>20)]
+print(col)
+print()
+col=df[(df["salary"]>10000) & (df["Age"]>20)]
+print(col)
---
+++ pandas/pandas_modification.py
@@ -0,0 +1,29 @@
+import pandas as pd
+data={
+    "Name":["ali","ahmad","kamal","talwar khan","azad khan"],
+    "Age":[23,21,22,21,22],
+    "City":["lahore","karachi","Talwandi","kasur","islamabad"],
+    "salary":[2000,23000,56000,45000,10000]
+}
+
+df=pd.DataFrame(data)
+print(df)
+df.insert(0,"id",["A1","A2","A3","A4","A5"])
+print("\n with index to chosse loction\n",df)
+
+# this is called straight forword method used when you are not inserting in that where your column be create :
+df["bounce"]=df["salary"] * 0.1
+print("\n with no location\n",df)
+print()
+
+# now we update the value,,,,where 2 is row index or os column ka name kis ki row ka index ha = new value jo ky 24 ha
+df.loc[2,"Age"]=24
+print(df)
+
+# now to remove :::
+df.drop(columns=["id"],inplace=True)
+print(df)
+print()
+# now remove multiple columns:
+df.drop(columns=["id","bounce"],inplace=True)
+print(df)
